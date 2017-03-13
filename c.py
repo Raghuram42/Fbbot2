6fb49a930fd8f6bf6be1306f53b6e2419479ed00
Raghuram42 <voruganti.raghuram@gmail.com>
2017-03-13 22:20:17 +0530

    Added BotMaster Name

---
+++ c.py
@@ -0,0 +1,13 @@
+import os
+
+myObj = {
+    "name":"John",
+    "age":30,
+    "cars": [
+        { "name":"Ford", "models":[ "Fiesta", "Focus", "Mustang" ] },
+        { "name":"BMW", "models":[ "320", "X3", "X5" ] },
+        { "name":"Fiat", "models":[ "500", "Panda" ] }
+    ]
+ }
+if myObj.name :
+  print "hi"
